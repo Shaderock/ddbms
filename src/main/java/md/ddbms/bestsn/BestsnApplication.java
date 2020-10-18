08f55de5a2e35fea39bd11d41a5dc4edff6565e4
Shaderock <hdv98plus@gmail.com>
2020-10-18 19:18:32 +0300

    Initial commit

---
+++ src/main/java/md/ddbms/bestsn/BestsnApplication.java
@@ -0,0 +1,13 @@
+package md.ddbms.bestsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestsnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestsnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/ddbms/bestsn/BestsnApplicationTests.java
@@ -0,0 +1,13 @@
+package md.ddbms.bestsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestsnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
