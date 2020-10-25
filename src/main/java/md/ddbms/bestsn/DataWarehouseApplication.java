c2f6c718c0a82ae0765457066f6d2567c54c35d7
Shaderock <hdv98plus@gmail.com>
2020-10-25 13:36:41 +0200

    refactor project architecture (add rmi and proxy modules and rename bestsn into data_warehouse)

--- src/main/java/md/ddbms/bestsn/DataWarehouseApplication.java
+++ src/main/java/md/ddbms/bestsn/DataWarehouseApplication.java
@@ -2,10 +2,8 @@ package md.ddbms.bestsn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(exclude = {
@@ -13,10 +11,10 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class
 })
-public class BestsnApplication {
+public class DataWarehouseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BestsnApplication.class, args);
+        SpringApplication.run(DataWarehouseApplication.class, args);
     }
 
 }
