78851c325126b162e4c4e396fe610a9beb543c49
Shaderock <hdv98plus@gmail.com>
2020-10-30 07:48:10 +0200

    fix data warehouse trying to autoconfigure redis

--- data_warehouse/src/main/java/md/ddbms/data_warehouse/DataWarehouseApplication.java
+++ data_warehouse/src/main/java/md/ddbms/data_warehouse/DataWarehouseApplication.java
@@ -3,6 +3,7 @@ package md.ddbms.data_warehouse;
 import md.ddbms.rmi.config.settings.server.ServerPortSettingsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class})
+        HibernateJpaAutoConfiguration.class,
+        RedisAutoConfiguration.class})
 @Import(ServerPortSettingsConfig.class)
 public class DataWarehouseApplication {
 
